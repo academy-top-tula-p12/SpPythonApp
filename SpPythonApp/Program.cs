23e0a97b596fe412dfbebbc3a5e0d31cf1ea75ff
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-06-29 09:56:48 +0300

    Добавьте файлы проекта.

---
+++ SpPythonApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+
+string codePython = "print('Hello world')";
+string filePython = "hello.py";
+
+ScriptEngine engine = Python.CreateEngine();
+//engine.Execute(codePython);
+
+//int x = 20;
+//ScriptScope scope = engine.CreateScope();
+//scope.SetVariable("b", x);
+//engine.ExecuteFile(filePython, scope);
+//dynamic y = scope.GetVariable("a");
+//Console.WriteLine($"a = {y}");
+
+ScriptScope scope = engine.CreateScope();
+engine.ExecuteFile(filePython, scope);
+
+//dynamic power = scope.GetVariable("power");
+//dynamic printStars = scope.GetVariable("printStars");
+
+//dynamic result = power(5, 4);
+//Console.WriteLine(result);
+//printStars(10);
+
+dynamic math = scope.GetVariable("math");
+Console.WriteLine(math.sin(5));
---
+++ SpPythonApp/hello.py
@@ -0,0 +1,13 @@
+﻿import math
+
+def power(x, n):
+	result = 1
+	for i in range(n):
+		result *= x
+	return result
+
+def printStars(n):
+	s = ""
+	for i in range(n) :
+		s += "*"
+	print(s)
